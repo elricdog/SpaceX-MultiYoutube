82781ed9b5f7856c80c53287895c10e9ec31f363
Ferran Ribell <fribell@opentrends.net>
2021-01-30 17:57:08 +0100

    Refactor code and remove double initializacion

--- js/firebaseFunctions.js
+++ js/firebaseFunctions.js
@@ -1,13 +1,16 @@
-// Set the configuration for your app
- // TODO: Replace with your project's config object
-var config = {
- apiKey: "AIzaSyDF-bCz0fuvpWEBFYN9l6uqtunG-gN_lRI",
- authDomain: "spacexboard.firebaseapp.com",
- databaseURL: "https://spacexboard-default-rtdb.firebaseio.com/",
- storageBucket: "bucket.appspot.com"
+var firebaseConfig = {
+  apiKey: "AIzaSyDF-bCz0fuvpWEBFYN9l6uqtunG-gN_lRI",
+  authDomain: "spacexboard.firebaseapp.com",
+  projectId: "spacexboard",
+  storageBucket: "spacexboard.appspot.com",
+  messagingSenderId: "310812025808",
+  appId: "1:310812025808:web:f9ad53c435580ce6f0cfd5",
+  databaseURL: "https://spacexboard-default-rtdb.firebaseio.com/",
+  measurementId: "G-740F453PWR"
 };
-firebase.initializeApp(config);
-
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
 // Get a reference to the database service
 var database = firebase.database();
 
