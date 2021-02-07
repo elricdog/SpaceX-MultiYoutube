fc81f0c3375727a31a2a5d581cf851f2021bd01c
Ferran Ribell <fribell@opentrends.net>
2021-02-07 11:45:42 +0100

    try to fix cast problems

--- js/cast_script.js
+++ js/cast_script.js
@@ -1,19 +1,18 @@
 var LOG_NAMESPACE = "urn:x-cast:elricdog.github.io.spacexboard.Log"
-if(typeof cast !== "undefined"){
-  var castReceiverContext = cast.framework.CastReceiverContext.getInstance();
-  //castReceiverContext.setLoggerLevel(cast.framework.LoggerLevel.DEBUG);
-  castReceiverContext.addCustomMessageListener(LOG_NAMESPACE, function (customEvent) {
-  logElement.innerText += LOG_NAMESPACE + " - " + customEvent.data.message + "\n";
-  console.log(LOG_NAMESPACE + " - " + customEvent.data.message);
-  });
-  var logElement = document.querySelector("#logger");
-  logElement.innerText = "Logging Events\n\n";
 
-  // JSON is the default type, but you can be sure anyway
-  castReceiverOptions.customNamespaces = Object.assign({});
-  castReceiverOptions.customNamespaces[LOG_NAMESPACE] = cast.framework.system.MessageType.JSON;
-  castReceiverContext.start(castReceiverOptions);
-}
+var castReceiverContext = cast.framework.CastReceiverContext.getInstance();
+//castReceiverContext.setLoggerLevel(cast.framework.LoggerLevel.DEBUG);
+castReceiverContext.addCustomMessageListener(LOG_NAMESPACE, function (customEvent) {
+logElement.innerText += LOG_NAMESPACE + " - " + customEvent.data.message + "\n";
+console.log(LOG_NAMESPACE + " - " + customEvent.data.message);
+});
+var logElement = document.querySelector("#logger");
+logElement.innerText = "Logging Events\n\n";
+
+// JSON is the default type, but you can be sure anyway
+castReceiverOptions.customNamespaces = Object.assign({});
+castReceiverOptions.customNamespaces[LOG_NAMESPACE] = cast.framework.system.MessageType.JSON;
+castReceiverContext.start(castReceiverOptions);
 
 
 window.addEventListener("load", function(){
