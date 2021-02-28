a0edb67a94813ca15f19513c849eaea00472a605
Ferran Ribell <fribell@opentrends.net>
2021-02-28 20:24:04 +0100

    fix log issue

--- js/cast_script.js
+++ js/cast_script.js
@@ -3,7 +3,7 @@ var LOG_NAMESPACE = "urn:x-cast:elricdog.github.io.spacexboard.Log"
 var castReceiverContext = cast.framework.CastReceiverContext.getInstance();
 //castReceiverContext.setLoggerLevel(cast.framework.LoggerLevel.DEBUG);
 castReceiverContext.addCustomMessageListener(LOG_NAMESPACE, function (customEvent) {
-  logElement.innerText += LOG_NAMESPACE + " - " + customEvent.data + "\n";
+  //logElement.innerText += LOG_NAMESPACE + " - " + customEvent.data + "\n";
   if(customEvent.data.videoId == ""){
     changeVolume(customEvent.data.position)
   }else{
@@ -11,8 +11,8 @@ castReceiverContext.addCustomMessageListener(LOG_NAMESPACE, function (customEven
   }
 
 });
-//var logElement = document.querySelector("#logger");
-//logElement.innerText = "Logging Events\n\n";
+// var logElement = document.querySelector("#logger");
+// logElement.innerText = "Logging Events\n\n";
 
 // JSON is the default type, but you can be sure anyway
 const options = new cast.framework.CastReceiverOptions();
